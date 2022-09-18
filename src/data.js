f8d25bdfd3e43ce4b9d54b100b4738313c9b028c
Manik-Chauhan <manik.mc58@gmail.com>
2022-09-19 00:58:39 +0530

    v1

---
+++ src/data.js
@@ -0,0 +1,44 @@
+export const projects = [
+  {
+    title: "Social Media App",
+    subtitle: "MERN Stack",
+    description:
+      "A meme and memory sharing social media app like instagram.",
+    image: "./memeo.png",
+    link: "https://github.com/Manik-Chauhan/Memeography",
+  },
+  {
+    title: "Yolo Chat",
+    subtitle: "HTML,CSS,JS and Socket.io",
+    description:
+      "A real time chat application that supports many users to chat simultaneously.",
+    image: "./project-3.gif",
+    link: "https://github.com/Manik-Chauhan/Yolo-Chat",
+  },
+  {
+    title: "News APP",
+    subtitle: "React",
+    description:
+      "Used newsAPI to get data and display various categories of news.",
+    image: "./github_logo.png",
+    link: "https://github.com/Manik-Chauhan/Yolo-News",
+  },
+  {
+    title: "Music Player App",
+    subtitle: "Java (Android)",
+    description:
+      "A basic music player app in android which displays and plays songs.",
+    image: "./tunexa.jpg",
+    link: "https://github.com/Manik-Chauhan/tunexa",
+  },
+];
+
+
+export const skills = [
+  "JavaScript",
+  "React",
+  "C++",
+  "Node",
+  "MongoDB",
+  "AWS",
+];
